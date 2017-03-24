e5ce352b1769e8fbdbf292c0702000ec20ca7e71
Svyatoslav <tantalishe@gmail.com>
2017-03-24 16:36:54 +0300

    fix variables

--- hist_picture.py
+++ hist_picture.py
@@ -10,12 +10,13 @@ image = cv2.imread(image_path, 1)
 
 
 cv2.imshow("original", image)
-image = cv2.medianBlur(image, 5)
-image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-hist = cv2.calcHist([image],[0],None,[256],[0,256])
+blurred = cv2.medianBlur(image, 5)
+gray = cv2.cvtColor(blurred, cv2.COLOR_BGR2GRAY)
+hist = cv2.calcHist([gray],[0],None,[256],[0,256])
 histogram = hp.get_hist_image(hist)
-cv2.imshow("processed", image)
+cv2.imshow("processed", gray)
 cv2.imshow("hist", histogram)
+
 
 # print hist.tolist().index(max(hist))
 
