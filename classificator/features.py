2bda4a428df2fa4e8fefb11ecba9e01f271edf50
tantalishe <tantalishe@gmail.com>
2017-11-14 15:32:06 +0300

    some minor fixes

--- classificator/features.py
+++ classificator/features.py
@@ -26,31 +26,38 @@ def finding_hu_M(contour):
 	HuM = cv2.HuMoments(M)
 	return HuM
 
+def f_humoments(cnt):
+	feature_vector = []
+	HuM = finding_hu_M(cnt)
+	for i in range(7):
+		feature_vector.append(HuM[i,0])
+	return feature_vector
+
+def f_standart(cnt):
+	circle = getSampleContour("data/dataset1/test/circle.jpg")
+
+	rect = cv2.minAreaRect(cnt)
+	points = cv2.boxPoints(rect)
+	x1, y1 = rect[0]
+	w, h = rect[1]
+	x2, y2 = centroid(cnt)
+	dist = dst(x1, y1, x2, y2)
+
+	dist_feature = dist / max(rect[1]) # DISTANCE BETWEEN CENTROID AND CENTER MINAREARECT
+	proportion_feature = max(w,h) / min(w,h) # ASPECT RATIO OF MINAREARECT
+	eps_circle_feature = cv2.matchShapes(cnt, circle, 1, 0) # SLOZHNO
+
+	feature_vector = [dist_feature, proportion_feature, eps_circle_feature]
+	return feature_vector
 
 def finding_features(cnt, ftype = "standart"):
-	feature_vector = []
 
 	if ftype == "humoments":
-		HuM = finding_hu_M(cnt)
-		for i in range(7):
-			feature_vector.append(HuM[i,0])
+		feature_vector = f_humoments(cnt)
 
 	elif ftype == "standart":
-		circle = getSampleContour("data/dataset1/test/circle.jpg")
+		feature_vector = f_standart(cnt)
 
-		rect = cv2.minAreaRect(cnt)
-		points = cv2.boxPoints(rect)
-		x1, y1 = rect[0]
-		w, h = rect[1]
-		x2, y2 = centroid(cnt)
-		dist = dst(x1, y1, x2, y2)
-
-		dist_feature = dist / max(rect[1]) # DISTANCE BETWEEN CENTROID AND CENTER MINAREARECT
-		proportion_feature = max(w,h) / min(w,h) # ASPECT RATIO OF MINAREARECT
-		eps_circle_feature = cv2.matchShapes(cnt, circle, 1, 0) # SLOZHNO
-
-		feature_vector = [dist_feature, proportion_feature, eps_circle_feature]
-	
 	return feature_vector
 
 
