c7db8dce627f4c02b722a3313c0d5f33fd26246b
tantalishe <tantalishe@gmail.com>
2018-04-13 20:35:27 +0300

    Add shuffling to dataset maker

--- classificator/make_sklearn_dataset.py
+++ classificator/make_sklearn_dataset.py
@@ -11,14 +11,15 @@ file_path_list = ["data/dataset1/scew_test/", "data/dataset1/nut/", "data/datase
 file_saving_path = "data/"
 
 
-data_list = [] # MAKE TRAIN DATASET
+data_list = [] # MAKE DATASET
 target_list = []
+NUMBER_EXAMPLES = NUMBER_TRAINING_EXAMPLES + NUMBER_TEST_EXAMPLES
 
 for target_number in range(NUMBER_CLASSES):
 
 	file_path = file_path_list[target_number]
 
-	for n in range(NUMBER_TRAINING_EXAMPLES):
+	for n in range(NUMBER_EXAMPLES):
 		
 		number = n + 1 # LOADING CONTOUR
 		filename = file_path + str(number) + ".npz"
@@ -31,32 +32,20 @@ for target_number in range(NUMBER_CLASSES):
 
 data_list = np.asarray(data_list) # TRANSFORMING INTO NP FORMAT
 target_list = np.asarray(target_list)
+s = np.arange(NUMBER_EXAMPLES*NUMBER_CLASSES) # SHUFFLE FULL DATASET
+data_list[:] = data_list[s[:]]
+target_list[:] = target_list[s[:]]
+
+train_data_list = data_list[:NUMBER_TRAINING_EXAMPLES] # SEPARATE FULL SET TO
+train_target_list = target_list[:NUMBER_TRAINING_EXAMPLES] # TRAIN AND TEST SETS
+test_data_list = data_list[-NUMBER_TEST_EXAMPLES:]
+test_target_list = target_list[-NUMBER_TEST_EXAMPLES:]
+
+
 np.save(file_saving_path + 'cnt_data', data_list) # SAVIN
 np.save(file_saving_path + 'cnt_targets', target_list)
 print("Train dataset done")
 
-
-
-test_data_list = [] # MAKE TEST DATASET
-test_target_list = []
-
-for target_number in range(NUMBER_CLASSES):
-
-	file_path = file_path_list[target_number]
-
-	for n in range(NUMBER_TEST_EXAMPLES):
-		
-		number = NUMBER_TRAINING_EXAMPLES + n + 1 # LOADING CONTOUR
-		filename = file_path + str(number) + ".npz"
-		data = np.load(filename)
-		cnt = data['arr_0']
-
-		feature_vector = ft.finding_features(cnt, ftype = FEATURE_TYPE) # FINDING FEATURES AND APPEND IT TO SET
-		test_data_list.append(feature_vector)
-		test_target_list.append(target_number)
-
-test_data_list = np.asarray(test_data_list) 
-test_target_list = np.asarray(test_target_list)
 np.save(file_saving_path + 'cnt_test_data', test_data_list) 
 np.save(file_saving_path + 'cnt_test_targets', test_target_list)
 print("Test dataset done")
