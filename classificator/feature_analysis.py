6fe6511d85b14b1651a11a7c858d86d786f4224e
tantalishe <tantalishe@gmail.com>
2018-05-03 17:12:12 +0300

    fix making dataset and feature analysis

--- classificator/feature_analysis.py
+++ classificator/feature_analysis.py
@@ -1,13 +1,15 @@
 import numpy as np
 import cv2
 import features as ft
+import matplotlib.pyplot as plt
+from matplotlib.figure import Figure
 
-NUMBER_TRAINING_EXAMPLES = 150
-NUMBER_TEST_EXAMPLES = 10
+NUMBER_TRAINING_EXAMPLES = 280
+NUMBER_TEST_EXAMPLES = 120
 NUMBER_CLASSES = 4
 FEATURE_TYPE = "humoments"
-VISUALISATION = 1 # TODO make graphics (2)
-file_path_list = ["data/dataset1/scew_test/", "data/dataset1/nut/", "data/dataset1/profile_20/", "data/dataset1/profile_40/"]
+VISUALISATION = 0 # 1 - console mean and std for every class and feature; 2 - graphics
+file_path_list = ["data/dataset3/scew/", "data/dataset3/nut/", "data/dataset3/profile_40/", "data/dataset3/profile_20/"]
 if FEATURE_TYPE == "humoments":
 	NUMBER_FEATURES = 7
 elif FEATURE_TYPE == "standart":
@@ -45,3 +47,29 @@ if VISUALISATION == 1:
 			print ("for feature number", f, "   M =", "{:.3E}".format(data_analysed[c, f, 0]), "   STD =", "{:.3E}".format(data_analysed[c, f, 1]))
 		print()
 
+color = ('xkcd:orangered','xkcd:turquoise','xkcd:lime green','xkcd:indigo')
+file_path_list = ["Болт", "Гайка", "Профиль 40 мм", "Профиль 20 мм"]
+# f = 1
+# print(data_features[1, 1])
+for f in range(NUMBER_FEATURES):
+	# fig = Figure()
+	for c in range(NUMBER_CLASSES):
+		plt.figure(1)
+		histr, bins, _ = plt.hist(data_features[c, f], facecolor=color[c])
+		print(np.sum(np.array(histr/NUMBER_TRAINING_EXAMPLES)))
+		print(bins)
+		print()
+		plt.close()
+		plt.figure(2)
+		plt.subplot(420+f+1)
+		bins_axis = np.zeros((len(bins) + 1))
+		bins_axis[0] = bins[0]
+		bins_axis[-1] = bins[-1]
+		histr = np.insert(histr, 0, 0)
+		histr = np.append(histr, 0)
+		for i in range(len(bins) - 1):
+			bins_axis[i+1] = (bins[i] + bins[i+1]) / 2 
+		plt.plot(bins_axis, histr/NUMBER_TRAINING_EXAMPLES, color[c], label=file_path_list[c])
+		plt.title('Момент номер '+str(f+1))
+		plt.legend()
+plt.show()
--- classificator/make_sklearn_dataset.py
+++ classificator/make_sklearn_dataset.py
@@ -7,7 +7,7 @@ NUMBER_TRAINING_EXAMPLES = 280
 NUMBER_TEST_EXAMPLES = 120
 NUMBER_CLASSES = 4
 FEATURE_TYPE = "humoments"
-file_path_list = ["data/dataset2/scew/", "data/dataset2/nut/", "data/dataset2/profile_20/", "data/dataset2/profile_40/"]
+file_path_list = ["data/dataset3/scew/", "data/dataset3/nut/", "data/dataset3/profile_20/", "data/dataset3/profile_40/"]
 file_saving_path = "data/"
 
 
@@ -36,6 +36,9 @@ s = np.arange(NUMBER_EXAMPLES*NUMBER_CLASSES) # SHUFFLE FULL DATASET
 np.random.shuffle(s)
 data_list[:] = data_list[s[:]]
 target_list[:] = target_list[s[:]]
+
+NUMBER_TRAINING_EXAMPLES = NUMBER_TRAINING_EXAMPLES*NUMBER_CLASSES
+NUMBER_TEST_EXAMPLES = NUMBER_TEST_EXAMPLES*NUMBER_CLASSES
 
 train_data_list = data_list[:NUMBER_TRAINING_EXAMPLES] # SEPARATE FULL SET TO
 train_target_list = target_list[:NUMBER_TRAINING_EXAMPLES] # TRAIN AND TEST SETS
